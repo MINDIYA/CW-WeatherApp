716502e447df6a96cb0ba1eab4399fbf45f5afd0
Mindiya Maitipe <114866147+MINDIYA@users.noreply.github.com>
2025-01-11 13:52:19 +0530

    Initial Commit

---
+++ CW-WeatherApp/CW_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CW_WeatherAppApp.swift
+//  CW-WeatherApp
+//
+//  Created by Mindiya Maitipe on 2025-01-11.
+//
+
+import SwiftUI
+
+@main
+struct CW_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CW-WeatherAppTests/CW_WeatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CW_WeatherAppTests.swift
+//  CW-WeatherAppTests
+//
+//  Created by Mindiya Maitipe on 2025-01-11.
+//
+
+import Testing
+@testable import CW_WeatherApp
+
+struct CW_WeatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
